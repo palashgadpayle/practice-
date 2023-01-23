c804360aa0db28c080b2ebff9bd79b065b76a223
palash <root@Palash.localdomain>
2023-01-23 22:34:55 +0530

    update

--- tomcat.groovy
+++ tomcat.groovy
@@ -28,7 +28,7 @@ pipeline {
             steps { 
                 withCredentials([sshUserPrivateKey(credentialsId: 'new-id', keyFileVariable: 'agentkey', usernameVariable: 'ubuntu')]) {            
                 sh '''
-                ssh -i ${tomcat} -o StrictHostKeyChecking=no ubuntu@54.95.209.119<<EOF
+                ssh -i ${agentkey} -o StrictHostKeyChecking=no ubuntu@54.95.209.119<<EOF
                 sudo apt-get update -y
                 sudo apt install unzip -y
                 #curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
