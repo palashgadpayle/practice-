654673f715e25d00ca67c3cc599e3266f8a6ade1
palash <root@Palash.localdomain>
2023-01-23 22:39:49 +0530

    update

--- tomcat.groovy
+++ tomcat.groovy
@@ -31,9 +31,9 @@ pipeline {
                 ssh -i ${agentkey} -o StrictHostKeyChecking=no ubuntu@54.95.209.119<<EOF
                 sudo apt-get update -y
                 sudo apt install unzip -y
-                #curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-                #unzip awscliv2.zip
-                #sudo ./aws/install
+                curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+                unzip awscliv2.zip
+                sudo ./aws/install
                 aws s3 cp s3://studentngpbckt/student-${BUILD_ID}.war .
                 curl -O https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.85/bin/apache-tomcat-8.5.85.tar.gz
                 sudo tar -xvf apache-tomcat-8.5.85.tar.gz -C /opt/
