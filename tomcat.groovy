bf16b443494d43dab00b6041f400c9ef664295aa
palash <root@Palash.localdomain>
2023-01-24 11:08:36 +0530

    new update

--- tomcat.groovy
+++ tomcat.groovy
@@ -34,13 +34,12 @@ pipeline {
                 #curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
                 #unzip awscliv2.zip
                 #sudo ./aws/install
-                aws s3 cp s3://tomcat-bucket-1/student-${BUILD_ID}.war .
-                sh 'pwd'
+                aws s3 cp s3://tomcat-bucket-1/student-${BUILD_ID}.war /home/ubuntu/
                 curl -O https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.85/bin/apache-tomcat-8.5.85.tar.gz
                 sudo tar -xvf apache-tomcat-8.5.85.tar.gz -C /home/ubuntu
                 sudo sh /home/ubuntu/apache-tomcat-8.5.85/bin/shutdown.sh
-                sudo cp -rv student-${BUILD_ID}.war studentapp.war
-                sudo cp -rv studentapp.war /home/ubuntu/apache-tomcat-8.5.85/webapps/
+                sudo cp -rv /home/ubuntu/student-${BUILD_ID}.war /home/ubuntu/studentapp.war
+                sudo cp -rv /home/ubuntu/studentapp.war /home/ubuntu/apache-tomcat-8.5.85/webapps/
                 sudo sh /home/ubuntu/apache-tomcat-8.5.85/bin/startup.sh
                 '''
                 }
